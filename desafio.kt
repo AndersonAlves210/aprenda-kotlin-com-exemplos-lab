7600a76b0b47e39918a8f20098e6f05dc84355a8
Andy <112736143+AndersonAlves210@users.noreply.github.com>
2024-09-17 00:18:39 -0300

    Atualizando o código com as implementações propostas

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,50 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
+fun imprimirFormacao(formacao: Formacao) {
+    println("Formação: ${formacao.nome}")
+    println("Conteúdo:")
+    for (conteudo in formacao.conteudos) {
+        println(" [${conteudo.nome} | duração: ${conteudo.duracao}h]")
+    }
+    println()
+}
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando um usuário
+    val usuario1 = Usuario()
+    
+    // Simulando a criação de uma Formação Básica
+    val conteudoBasico1 = ConteudoEducacional("Aprendendo a sintaxe", 2)
+    val conteudoBasico2 = ConteudoEducacional("Criando seu primeiro código", 1)
+    val formacao1 = Formacao("Kotlin Básico", listOf(conteudoBasico1, conteudoBasico2), Nivel.BASICO)
+    imprimirFormacao(formacao1)
+    
+    // Simulando a criação de uma Formação Intermediária
+    val conteudoIntermediario1 = ConteudoEducacional("Manipulando variáveis", 4)
+    val conteudoIntermediario2 = ConteudoEducacional("Utilizando Classes", 5)
+    val formacao2 = Formacao("Kotlin Intermediário", listOf(conteudoIntermediario1, conteudoIntermediario2), Nivel.INTERMEDIARIO)
+    imprimirFormacao(formacao2)
+    
+    // Simulando a criação de uma Formação Avançada
+    val conteudoAvancado1 = ConteudoEducacional("Programação Funcional", 20)
+    val conteudoAvancado2 = ConteudoEducacional("Coroutines e Programação Assíncrona", 20)
+    val formacao3 = Formacao("Kotlin Avançado", listOf(conteudoAvancado1, conteudoAvancado2), Nivel.AVANCADO)
+    imprimirFormacao(formacao3)
+    
+    // Testando a função matricular
+    formacao1.matricular(usuario1)
+    println("Usuário matriculado com sucesso.")
 }
